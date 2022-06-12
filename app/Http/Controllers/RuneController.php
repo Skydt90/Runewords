501b3639e465e922ab24d85bfb60a4a8d6f2ed05
Skydt90 <christian_skydt@hotmail.comgit config --global user.email christian_skydt@hotmail.com>
2022-06-12 20:05:13 +0200

    Cache controller responses

--- app/Http/Controllers/RuneController.php
+++ app/Http/Controllers/RuneController.php
@@ -5,18 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Rune;
 use App\Models\RuneRecipe;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Cache;
 
 class RuneController extends Controller
 {
     public function index(): View
     {
-        $runes = Rune::all()->map(function ($rune) {
-            $attr = explode('|', $rune->attributes);
-            unset($rune->attributes);
+        $runes = Cache::remember('runes', now()->addHour(), function () {
 
-            $rune->attributes = $attr;
+            return Rune::all()->map(function ($rune) {
+                $attr = explode('|', $rune->attributes);
+                unset($rune->attributes);
 
-            return $rune;
+                $rune->attributes = $attr;
+
+                return $rune;
+            });
         });
 
         return view('runes.index', ['runes' => $runes]);
@@ -26,9 +30,11 @@ class RuneController extends Controller
     {
         $fields = ['id', 'rune_recipe_id', 'image'];
 
-        $runeRecipes = RuneRecipe::with(['runes' => fn ($q) => $q->select($fields)])
-            ->with(['gem' => fn ($q) => $q->select($fields)])
-            ->get();
+        $runeRecipes = Cache::remember('rune-recipes', now()->addHour(), function () use ($fields) {
+            return RuneRecipe::with(['runes' => fn ($q) => $q->select($fields)])
+                ->with(['gem' => fn ($q) => $q->select($fields)])
+                ->get();
+        });
 
         return view('runes.recipes', ['runeRecipes' => $runeRecipes]);
     }
--- app/Http/Controllers/RunewordController.php
+++ app/Http/Controllers/RunewordController.php
@@ -4,47 +4,54 @@ namespace App\Http\Controllers;
 
 use App\Models\Rune;
 use App\Models\Runeword;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class RunewordController extends Controller
 {
     public function index(): View
     {
-        $runewords = Runeword::orderBy('level', 'asc')
-            ->with('runes')
-            ->get()
-            ->map(function ($runeword) {
-                $attr = explode('|', $runeword->attributes);
-                unset($runeword->attributes);
+        $runewords = Cache::remember('runewords', now()->addHour(), function () {
+            return Runeword::orderBy('level', 'asc')
+                ->with('runes')
+                ->get()
+                ->map(function ($runeword) {
+                    $attr = explode('|', $runeword->attributes);
+                    unset($runeword->attributes);
 
-                $runeword->attributes = $attr;
+                    $runeword->attributes = $attr;
 
-                return $runeword;
-            });
+                    return $runeword;
+                });
+        });
 
         return view('runewords.index', ['runewords' => $runewords]);
     }
 
     public function calculator(): View
     {
-        $runewords = Runeword::with(['runes' => fn ($query) => $query->select('runes.id')])
-            ->get()
-            ->map(function ($rWord) {
+        $runewords = Cache::remember('calculator-runewords', now()->addHour(), function () {
+            return Runeword::with(['runes' => fn ($query) => $query->select('runes.id')])
+                ->get()
+                ->map(function ($rWord) {
 
-                $runes = $rWord->runes->pluck('id')->toArray();
-                $attr = explode('|', $rWord->attributes);
+                    $runes = $rWord->runes->pluck('id')->toArray();
+                    $attr = explode('|', $rWord->attributes);
 
-                unset($rWord->runes);
-                unset($rWord->attributes);
+                    unset($rWord->runes);
+                    unset($rWord->attributes);
 
-                $rWord->runes = $runes;
-                $rWord->attributes = $attr;
+                    $rWord->runes = $runes;
+                    $rWord->attributes = $attr;
 
-                return $rWord;
+                    return $rWord;
 
-            })->toArray();
+                })->toArray();
+        });
 
-        $runes = Rune::orderBy('level', 'asc')->get();
+        $runes = Cache::remember('calculator-runes', now()->addHour(), function () {
+            return Rune::orderBy('level', 'asc')->get();
+        });
 
         return view('runewords.calculator', ['runes' => $runes, 'runewords' => $runewords]);
     }
